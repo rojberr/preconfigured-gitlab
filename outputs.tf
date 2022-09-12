fc0f9915a03ea781793ef3402036e42ecf84956a
Rojberr <rojberr@outlook.com>
2022-09-12 21:30:23 +0200

    build(terra): Export variables to variables.tf file

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 output "ec2instance" {
-  value       = "http://${aws_instance.preconfigured-gitlab.public_ip}"
+  value       = "http://${aws_instance.preconfigured-gitlab.public_ip}/"
   description = "The public IP address of your EC2 instance"
 }
